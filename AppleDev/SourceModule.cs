b42558e791a143f232ab96af208ec71f3ea39a2a
notjulie <rasmussenrandy189@gmail.com>
2020-05-10 12:09:10 -0500

    Another step in the top-down process of tokenizing.

--- AppleDev/SourceModule.cs
+++ AppleDev/SourceModule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,11 @@ namespace AppleDev
       {
          get;
          private set;
+      }
+
+      internal TextReader OpenReader()
+      {
+         return new StringReader(ModuleText);
       }
    }
 }
--- AppleDev/Tokens/Tokenizer.cs
+++ AppleDev/Tokens/Tokenizer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,31 @@ namespace AppleDev.Tokens
 {
    class Tokenizer
    {
+      /// <summary>
+      /// Creates a TokenizedModule for the SourceModule
+      /// </summary>
+      /// <param name="sourceModule"></param>
+      /// <returns></returns>
       public TokenizedModule Tokenize(SourceModule sourceModule)
       {
-         throw new NotImplementedException("Tokenizer.Tokenize");
+         TokenizedModule result = new TokenizedModule();
+
+         using (TextReader reader = sourceModule.OpenReader())
+         {
+            for (; ; )
+            {
+               string line = reader.ReadLine();
+               if (line == null)
+                  return result;
+
+               TokenizeLine(line);
+            }
+         }
+      }
+
+      private void TokenizeLine(string line)
+      {
+         throw new NotImplementedException("Tokenizer.TokenizeLine");
       }
    }
 }
