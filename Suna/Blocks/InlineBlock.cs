40f3f67e0667c1e87db4e35104151437a76327d1
notjulie <rasmussenrandy189@gmail.com>
2020-05-19 20:58:23 -0500

    Implemented the compilation of InlineBlock objects... without support for parameters yet.

--- Suna/Blocks/InlineBlock.cs
+++ Suna/Blocks/InlineBlock.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +11,52 @@ using Suna.Tokens;
 
 namespace Suna.Blocks
 {
+   /// <summary>
+   /// Represents a block in the code that starts with the "inline" keyword.  It's
+   /// a function type syntax that is evaluated somewhat like a C macro expansion.
+   /// </summary>
    class InlineBlock : Block
    {
+      /// <summary>
+      /// Initializes a new instance of class InlineBlock
+      /// </summary>
+      /// <param name="tokens"></param>
       public InlineBlock(Token[] tokens)
          :base(tokens)
       {
          Name = ((IdentifierToken)tokens[1]).Identifier;
       }
 
+      /// <summary>
+      /// Gets the block's name
+      /// </summary>
       public string Name
       {
          get;
          private set;
       }
 
+      /// <summary>
+      /// Compiles the block to the given context with the given parameters
+      /// </summary>
+      /// <param name="context"></param>
+      /// <param name="callParameters"></param>
       public void Compile(LinkContext context, ParentheticGroup callParameters)
       {
-         throw new NotImplementedException();
+         Contract.Requires(context != null);
+         Contract.Requires(callParameters != null);
+
+         // groupify the contents of the block
+         Group groupedBlock = (Group)GroupedBlock.Read(this.Tokens.GetEnumerator());
+
+         // there should be 4 items in the group:
+         //   0: inline keyword
+         //   1: identifier token
+         //   2: parenthetical containing the parameter list
+         //   3: code block
+
+         // just compile the code for now
+         groupedBlock.Items.ElementAt(3).Compile(context);
       }
    }
 }
