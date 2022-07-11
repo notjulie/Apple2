d9ebd313558a14e535f453445f1a17850fb60699
notjulie <rasmussenrandy189@gmail.com>
2022-07-10 21:53:39 -0500

    An actual Apple ][ Hello World written in C.

--- Seahaven2e/src/main.c
+++ Seahaven2e/src/main.c
@@ -1,18 +1,23 @@
 
+#include <stdint.h>
 
 // there's no point in returning a result from main in an Apple][ program,
 // so that's just 3 wasted op codes
 #pragma clang diagnostic ignored "-Wmain-return-type"
+
+inline void putc(register uint8_t c)
+{
+   asm volatile ("JSR\t$FDED" : "+a"(c));
+}
 
 /** \brief
  * Main entry point
  */
 void main (void)
 {
-   typedef void voidFunction(void);
-   voidFunction *beep = (voidFunction *)0xFF3A;
    int i;
 
-   for (i=0; i<10; ++i)
-      beep();
+   char hello[] = "HELLO WORLD";
+   for (i=0; i<sizeof(hello) - 1; ++i)
+      putc(hello[i]);
 }
