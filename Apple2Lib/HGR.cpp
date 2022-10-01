564b22630bd6d8e9188837484a9bec3c7b1f2083
notjulie <rasmussenrandy189@gmail.com>
2022-10-01 17:45:09 -0500

    Documentation.

--- Apple2Lib/HGR.cpp
+++ Apple2Lib/HGR.cpp
@@ -1,4 +1,14 @@
 
+/// \file HGR.cpp
+///
+/// Author: Randy Rasmussen
+/// Copyright: 2022
+/// License: Public Domain
+/// Warranties: None
+///
+/// Utilities for Apple ][ HIRES graphics support
+///
+
 #include "HGR.h"
 #include <C6502/Memory.h>
 
--- Apple2Lib/HGR.h
+++ Apple2Lib/HGR.h
@@ -1,12 +1,27 @@
 
+/// \file HGR.h
+///
+/// Author: Randy Rasmussen
+/// Copyright: 2022
+/// License: Public Domain
+/// Warranties: None
+///
+/// Declarations for Apple ][ HIRES graphics support
+///
+
 #ifndef APPLE2HGR_H
 #define APPLE2HGR_H
 
 #include <stdint.h>
 
 namespace a2 {
+
+   /// \brief
+   ///    Height of the HGR screen in pixels
    const uint8_t HGRHeight = 192;
 
+   /// \brief
+   ///    Base address of HGR page 1
    inline uint8_t *HGRPage1Base() {
       return (uint8_t *)0x2000;
    }
