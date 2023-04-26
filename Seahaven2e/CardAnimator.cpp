ed4ebdf2fe441b780b3bd857a9ec42b5b86d97b8
notjulie <rasmussenrandy189@gmail.com>
2023-04-26 15:53:48 -0500

    Resolve cpplint warnings.

--- Seahaven2e/CardAnimator.cpp
+++ Seahaven2e/CardAnimator.cpp
@@ -1,3 +1,6 @@
+// =============================================================
+//    Copyright 2023 Randy Rasmussen
+// =============================================================
 
 #include "CardAnimator.h"
 
@@ -12,150 +15,139 @@ CardAnimator CardAnimator::instance;
 /// \brief
 ///   Starts an animation of a card from one position to another
 ///
-void CardAnimator::StartAnimation(Card card, CardLocation start, CardLocation end)
-{
-   // step 1: remove the card from its current position
-   Game::instance.SetCard(start, Card());
+void CardAnimator::StartAnimation(
+      Card card,
+      CardLocation start,
+      CardLocation end) {
+  // step 1: remove the card from its current position
+  Game::instance.SetCard(start, Card());
 
-   // set the bounds of the animation
-   currentX = start.GetX();
-   currentY = start.GetY();
-   targetX = end.GetX();
-   targetY = end.GetY();
-   if (targetX > currentX)
-   {
-      distanceX = targetX - currentX;
-      directionX = 1;
-   }
-   else
-   {
-      distanceX = currentX - targetX;
-      directionX = -1;
-   }
-   if (targetY > currentY)
-   {
-      distanceY = targetY - currentY;
-      directionY = 1;
-   }
-   else
-   {
-      distanceY = currentY - targetY;
-      directionY = -1;
-   }
-   // throw together a duration... TODO better
-   duration = 30;
-   timeLeft = duration;
-   numeratorX = numeratorY = 0;
-   lastVBLCount = a2::VBLCounter::GetCounter();
-   cardToMove = card;
+  // set the bounds of the animation
+  currentX = start.GetX();
+  currentY = start.GetY();
+  targetX = end.GetX();
+  targetY = end.GetY();
+  if (targetX > currentX) {
+    distanceX = targetX - currentX;
+    directionX = 1;
+  } else {
+    distanceX = currentX - targetX;
+    directionX = -1;
+  }
+  if (targetY > currentY) {
+    distanceY = targetY - currentY;
+    directionY = 1;
+  } else {
+    distanceY = currentY - targetY;
+    directionY = -1;
+  }
+  // throw together a duration... TODO better
+  duration = 30;
+  timeLeft = duration;
+  numeratorX = numeratorY = 0;
+  lastVBLCount = a2::VBLCounter::GetCounter();
+  cardToMove = card;
 
-   // draw the game without the card on page 2
-   drawing2.DrawBackground();
-   drawing2.DrawGame();
+  // draw the game without the card on page 2
+  drawing2.DrawBackground();
+  drawing2.DrawGame();
 
-   // draw the card at its original position, saving the background
-   background2X = currentX;
-   background2Y = currentY;
-   drawing2.SaveCardBackground(currentX, currentY, &background2);
-   drawing2.DrawCard(card, currentX, currentY);
+  // draw the card at its original position, saving the background
+  background2X = currentX;
+  background2Y = currentY;
+  drawing2.SaveCardBackground(currentX, currentY, &background2);
+  drawing2.DrawCard(card, currentX, currentY);
 
-   // switch to page 2
-   a2::PAGE2ON();
+  // switch to page 2
+  a2::PAGE2ON();
 
-   // set the state
-   state = State::Page2Initialized;
+  // set the state
+  state = State::Page2Initialized;
 }
 
 
 /// \brief
 ///   Updates the state of the animation
 ///
-void CardAnimator::Service()
-{
-   switch (state)
-   {
-   case State::Idle:
+void CardAnimator::Service() {
+  switch (state) {
+  case State::Idle:
+    break;
+
+  case State::Page2Initialized:
+    // copy page 2 to page 1
+    drawing2.CopyTo(drawing1);
+
+    // erase the card that we're moving
+    drawing1.RestoreBackground(background2, background2X, background2Y);
+
+    // set the new position
+    UpdatePosition();
+
+    // save background, draw, switch pages, update state
+    background1X = currentX;
+    background1Y = currentY;
+    drawing1.SaveCardBackground(currentX, currentY, &background1);
+    drawing1.DrawCard(cardToMove, currentX, currentY);
+    a2::PAGE2OFF();
+    state = State::Page1Visible;
+    break;
+
+  case State::Page1Visible:
+    if (timeLeft == 0) {
+      state = State::Idle;
       break;
+    }
+    drawing2.RestoreBackground(background2, background2X, background2Y);
+    UpdatePosition();
+    background2X = currentX;
+    background2Y = currentY;
+    drawing2.SaveCardBackground(currentX, currentY, &background2);
+    drawing2.DrawCard(cardToMove, currentX, currentY);
+    a2::PAGE2ON();
+    state = State::Page2Visible;
+    break;
 
-   case State::Page2Initialized:
-      // copy page 2 to page 1
-      drawing2.CopyTo(drawing1);
-
-      // erase the card that we're moving
-      drawing1.RestoreBackground(background2, background2X, background2Y);
-
-      // set the new position
-      UpdatePosition();
-
-      // save background, draw, switch pages, update state
-      background1X = currentX;
-      background1Y = currentY;
-      drawing1.SaveCardBackground(currentX, currentY, &background1);
-      drawing1.DrawCard(cardToMove, currentX, currentY);
-      a2::PAGE2OFF();
-      state = State::Page1Visible;
-      break;
-
-   case State::Page1Visible:
-      if (timeLeft == 0)
-      {
-         state = State::Idle;
-         break;
-      }
-      drawing2.RestoreBackground(background2, background2X, background2Y);
-      UpdatePosition();
-      background2X = currentX;
-      background2Y = currentY;
-      drawing2.SaveCardBackground(currentX, currentY, &background2);
-      drawing2.DrawCard(cardToMove, currentX, currentY);
-      a2::PAGE2ON();
-      state = State::Page2Visible;
-      break;
-
-   case State::Page2Visible:
-      drawing1.RestoreBackground(background1, background1X, background1Y);
-      UpdatePosition();
-      background1X = currentX;
-      background1Y = currentY;
-      drawing1.SaveCardBackground(currentX, currentY, &background1);
-      drawing1.DrawCard(cardToMove, currentX, currentY);
-      a2::PAGE2OFF();
-      state = State::Page1Visible;
-      break;
-   }
+  case State::Page2Visible:
+    drawing1.RestoreBackground(background1, background1X, background1Y);
+    UpdatePosition();
+    background1X = currentX;
+    background1Y = currentY;
+    drawing1.SaveCardBackground(currentX, currentY, &background1);
+    drawing1.DrawCard(cardToMove, currentX, currentY);
+    a2::PAGE2OFF();
+    state = State::Page1Visible;
+    break;
+  }
 }
 
 
 /// \brief
 ///   Calculates the new position
 ///
-void CardAnimator::UpdatePosition()
-{
-   if (timeLeft == 0)
-      return;
+void CardAnimator::UpdatePosition() {
+  if (timeLeft == 0)
+    return;
 
-   // snapshot the time
-   uint8_t now = a2::VBLCounter::GetCounter();
+  // snapshot the time
+  uint8_t now = a2::VBLCounter::GetCounter();
 
-   // step once for each tick of the clock
-   while (lastVBLCount != now)
-   {
-      numeratorX += distanceX;
-      while (numeratorX >= duration)
-      {
-         currentX += directionX;
-         numeratorX -= duration;
-      }
+  // step once for each tick of the clock
+  while (lastVBLCount != now) {
+    numeratorX += distanceX;
+    while (numeratorX >= duration) {
+      currentX += directionX;
+      numeratorX -= duration;
+    }
 
-      numeratorY += distanceY;
-      while (numeratorY >= duration)
-      {
-         currentY += directionY;
-         numeratorY -= duration;
-      }
+    numeratorY += distanceY;
+    while (numeratorY >= duration) {
+      currentY += directionY;
+      numeratorY -= duration;
+    }
 
-      ++lastVBLCount;
-      if (--timeLeft == 0)
-         break;
-   }
+    ++lastVBLCount;
+    if (--timeLeft == 0)
+      break;
+  }
 }
--- Seahaven2e/CardAnimator.h
+++ Seahaven2e/CardAnimator.h
@@ -1,55 +1,58 @@
+// =============================================================
+//    Copyright 2023 Randy Rasmussen
+// =============================================================
 
-#ifndef CARDANIMATOR_h
-#define CARDANIMATOR_h
+#ifndef SEAHAVEN2E_CARDANIMATOR_H_
+#define SEAHAVEN2E_CARDANIMATOR_H_
 
 #include "Card.h"
 #include "CardLocation.h"
 #include "SavedBackground.h"
 
 class CardAnimator {
-public:
-   bool IsAnimating() { return state != State::Idle; }
-   void StartAnimation(Card card, CardLocation start, CardLocation end);
-   void Service();
+ public:
+  bool IsAnimating() { return state != State::Idle; }
+  void StartAnimation(Card card, CardLocation start, CardLocation end);
+  void Service();
 
-public:
-   static CardAnimator instance;
+ public:
+  static CardAnimator instance;
 
-private:
-   enum class State {
-      Idle,
-      Page2Initialized,
-      Page1Visible,
-      Page2Visible
-   };
+ private:
+  enum class State {
+    Idle,
+    Page2Initialized,
+    Page1Visible,
+    Page2Visible
+  };
 
-private:
-   void UpdatePosition();
+ private:
+  void UpdatePosition();
 
-private:
-   State state = State::Idle;
+ private:
+  State state = State::Idle;
 
-   // saved background for graphics page 1
-   uint8_t background1X;
-   uint8_t background1Y;
-   SavedBackground background1;
+  // saved background for graphics page 1
+  uint8_t background1X;
+  uint8_t background1Y;
+  SavedBackground background1;
 
-   // saved background for graphics page 2
-   uint8_t background2X;
-   uint8_t background2Y;
-   SavedBackground background2;
+  // saved background for graphics page 2
+  uint8_t background2X;
+  uint8_t background2Y;
+  SavedBackground background2;
 
-   // the animation in progress
-   Card cardToMove;
-   uint8_t currentX, currentY;
-   uint8_t targetX, targetY;
-   uint8_t distanceX, distanceY;
-   int8_t directionX, directionY;
+  // the animation in progress
+  Card cardToMove;
+  uint8_t currentX, currentY;
+  uint8_t targetX, targetY;
+  uint8_t distanceX, distanceY;
+  int8_t directionX, directionY;
 
-   uint8_t numeratorX, numeratorY;
-   uint8_t lastVBLCount;
-   uint8_t duration;
-   uint8_t timeLeft;
+  uint8_t numeratorX, numeratorY;
+  uint8_t lastVBLCount;
+  uint8_t duration;
+  uint8_t timeLeft;
 };
 
-#endif // CARDANIMATOR_h
+#endif  // SEAHAVEN2E_CARDANIMATOR_H_
