580bb32a81eb93a3de0485cf1fafe41b1f1b1379
notjulie <rasmussenrandy189@gmail.com>
2023-04-26 16:33:57 -0500

    More cpplint

--- Seahaven2e/Drawing.cpp
+++ Seahaven2e/Drawing.cpp
@@ -1,3 +1,6 @@
+// =============================================================
+//    Copyright 2023 Randy Rasmussen
+// =============================================================
 
 #include "Drawing.h"
 
@@ -25,216 +28,199 @@ Drawing drawing2(a2::HGRPage::HGR2());
  * Draws the shape of a card at the given location in the main
  * HGR window
  */
-void Drawing::DrawCardBackground(uint8_t row, uint8_t byteOffset)
-{
-   {
-      uint8_t *rowPointer = hgr.GetByteAddress(row++, byteOffset);
-      rowPointer[0] = 0xFC;
-      rowPointer[1] = 0xFF;
-      rowPointer[2] = 0xFF;
-      rowPointer[3] = 0x1F;
-   }
+void Drawing::DrawCardBackground(uint8_t row, uint8_t byteOffset) {
+  {
+    uint8_t *rowPointer = hgr.GetByteAddress(row++, byteOffset);
+    rowPointer[0] = 0xFC;
+    rowPointer[1] = 0xFF;
+    rowPointer[2] = 0xFF;
+    rowPointer[3] = 0x1F;
+  }
 
-   for (uint8_t i=0; i<37; ++i)
-   {
-      uint8_t *rowPointer = hgr.GetByteAddress(row++, byteOffset);
-      rowPointer[0] = 0xFE;
-      rowPointer[1] = 0xFF;
-      rowPointer[2] = 0xFF;
-      rowPointer[3] = 0x3F;
-   }
+  for (uint8_t i=0; i < 37; ++i) {
+    uint8_t *rowPointer = hgr.GetByteAddress(row++, byteOffset);
+    rowPointer[0] = 0xFE;
+    rowPointer[1] = 0xFF;
+    rowPointer[2] = 0xFF;
+    rowPointer[3] = 0x3F;
+  }
 
-   {
-      uint8_t *rowPointer = hgr.GetByteAddress(row++, byteOffset);
-      rowPointer[0] = 0xFC;
-      rowPointer[1] = 0xFF;
-      rowPointer[2] = 0xFF;
-      rowPointer[3] = 0x1F;
-   }
+  {
+    uint8_t *rowPointer = hgr.GetByteAddress(row++, byteOffset);
+    rowPointer[0] = 0xFC;
+    rowPointer[1] = 0xFF;
+    rowPointer[2] = 0xFF;
+    rowPointer[3] = 0x1F;
+  }
 }
 
 
-void Drawing::DrawBackground()
-{
-   hgr.Fill(0);
+void Drawing::DrawBackground() {
+  hgr.Fill(0);
 }
 
 
-void Drawing::DrawSprite(const a2::HGRWord *sprite, uint8_t rows, uint8_t y, uint8_t x)
-{
-   for (int i=0; i<rows; ++i)
-   {
-      uint8_t *rowPointer = hgr.GetByteAddress(y++, x);
-      rowPointer[0] = sprite[i].GetLeft();
-      rowPointer[1] = sprite[i].GetRight();
-   }
+void Drawing::DrawSprite(
+                const a2::HGRWord *sprite,
+                uint8_t rows,
+                uint8_t y,
+                uint8_t x) {
+  for (int i=0; i < rows; ++i) {
+    uint8_t *rowPointer = hgr.GetByteAddress(y++, x);
+    rowPointer[0] = sprite[i].GetLeft();
+    rowPointer[1] = sprite[i].GetRight();
+  }
 }
 
 
-void Drawing::XorSprite(const a2::HGRWord *sprite, uint8_t rows, uint8_t y, uint8_t x)
-{
-   for (int i=0; i<rows; ++i)
-   {
-      uint8_t *rowPointer = hgr.GetByteAddress(y++, x);
-      rowPointer[0] ^= sprite[i].GetLeft();
-      rowPointer[1] ^= sprite[i].GetRight();
-   }
+void Drawing::XorSprite(
+                const a2::HGRWord *sprite,
+                uint8_t rows,
+                uint8_t y,
+                uint8_t x) {
+  for (int i=0; i < rows; ++i) {
+    uint8_t *rowPointer = hgr.GetByteAddress(y++, x);
+    rowPointer[0] ^= sprite[i].GetLeft();
+    rowPointer[1] ^= sprite[i].GetRight();
+  }
 }
 
 
-void Drawing::DrawCardTop(Card card, uint8_t x, uint8_t y)
-{
-   DrawSprite(Sprites::GetRankSprite(card.GetRank()), CardTopSpriteHeight, y, x);
-   DrawSprite(Sprites::GetSuitSprite(card.GetSuit()), CardTopSpriteHeight, y, x + 2);
+void Drawing::DrawCardTop(Card card, uint8_t x, uint8_t y) {
+  DrawSprite(Sprites::GetRankSprite(card.GetRank()), CardTopSpriteHeight, y, x);
+  DrawSprite(
+    Sprites::GetSuitSprite(card.GetSuit()),
+    CardTopSpriteHeight,
+    y, x + 2);
 
-   DrawCursor(x, y);
+  DrawCursor(x, y);
 }
 
 
-void Drawing::DrawCursor(uint8_t x, uint8_t y)
-{
-   XorSprite(Sprites::cursorLeft, CardTopSpriteHeight, y, x);
-   XorSprite(Sprites::cursorRight, CardTopSpriteHeight, y, x + 2);
+void Drawing::DrawCursor(uint8_t x, uint8_t y) {
+  XorSprite(Sprites::cursorLeft, CardTopSpriteHeight, y, x);
+  XorSprite(Sprites::cursorRight, CardTopSpriteHeight, y, x + 2);
 }
 
 
-void Drawing::DrawCard(Card card, uint8_t x, uint8_t y)
-{
-   a2::VBLCounter::Update();
-   DrawCardTop(card, x, y);
-   a2::VBLCounter::Update();
-   DrawCardBottom(x, y + CardTopSpriteHeight);
-   a2::VBLCounter::Update();
+void Drawing::DrawCard(Card card, uint8_t x, uint8_t y) {
+  a2::VBLCounter::Update();
+  DrawCardTop(card, x, y);
+  a2::VBLCounter::Update();
+  DrawCardBottom(x, y + CardTopSpriteHeight);
+  a2::VBLCounter::Update();
 }
 
-void Drawing::DrawCardBottom(uint8_t x, uint8_t y)
-{
-   uint8_t * row;
+void Drawing::DrawCardBottom(uint8_t x, uint8_t y) {
+  uint8_t * row;
 
-   for (uint8_t i=0; i<CardHeight - CardTopSpriteHeight - 1; ++i)
-   {
-      row = hgr.GetByteAddress(y++, x);
-      row[0] = 0xFF;
-      row[1] = 0xFF;
-      row[2] = 0xFF;
-      row[3] = 0xBF;
-   }
+  for (uint8_t i=0; i < CardHeight - CardTopSpriteHeight - 1; ++i) {
+    row = hgr.GetByteAddress(y++, x);
+    row[0] = 0xFF;
+    row[1] = 0xFF;
+    row[2] = 0xFF;
+    row[3] = 0xBF;
+  }
 
-   row = hgr.GetByteAddress(y++, x);
-   row[0] = 0xFE;
-   row[1] = 0xFF;
-   row[2] = 0xFF;
-   row[3] = 0x9F;
+  row = hgr.GetByteAddress(y++, x);
+  row[0] = 0xFE;
+  row[1] = 0xFF;
+  row[2] = 0xFF;
+  row[3] = 0x9F;
 }
 
 
-void Drawing::DrawAcePile(Suit suit, uint8_t x)
-{
-   Card card = Game::instance.GetAcePileCard(suit);
-   if (!card.IsNull())
-   {
+void Drawing::DrawAcePile(Suit suit, uint8_t x) {
+  Card card = Game::instance.GetAcePileCard(suit);
+  if (!card.IsNull()) {
       DrawCard(card, x, CardLocation::TowersTop);
-   }
+  }
 }
 
-void Drawing::DrawAcePiles()
-{
-   DrawAcePile(Suit::Clubs, 0);
-   DrawAcePile(Suit::Diamonds, 4);
-   DrawAcePile(Suit::Hearts, 32);
-   DrawAcePile(Suit::Spades, 36);
-   uint8_t x = TowersLeft;
+void Drawing::DrawAcePiles() {
+  DrawAcePile(Suit::Clubs, 0);
+  DrawAcePile(Suit::Diamonds, 4);
+  DrawAcePile(Suit::Hearts, 32);
+  DrawAcePile(Suit::Spades, 36);
+  uint8_t x = TowersLeft;
 
-   for (uint8_t tower=0; tower<4; ++tower)
-   {
-      Card card = Game::instance.GetTower(tower);
-      if (!card.IsNull())
-      {
-         DrawCard(card, x, CardLocation::TowersTop);
+  for (uint8_t tower=0; tower < 4; ++tower) {
+    Card card = Game::instance.GetTower(tower);
+    if (!card.IsNull()) {
+      DrawCard(card, x, CardLocation::TowersTop);
+    }
+    x += 4;
+  }
+}
+
+void Drawing::DrawTowers() {
+  uint8_t x = TowersLeft;
+
+  for (uint8_t tower=0; tower < 4; ++tower) {
+    Card card = Game::instance.GetTower(tower);
+    if (!card.IsNull()) {
+      DrawCard(card, x, CardLocation::TowersTop);
+    }
+    x += 4;
+  }
+}
+
+void Drawing::DrawColumns() {
+  uint8_t x = 0;
+
+  for (uint8_t i=0; i < 10; ++i) {
+    Column &column = Game::instance.GetColumn(i);
+
+    uint8_t y = CardLocation::ColumnsTop;
+    for (uint8_t j=0; j < 5; ++j) {
+      DrawCardTop(column.GetCard(j), x, y);
+      if (j == 4) {
+        DrawCardBottom(x, y + CardTopSpriteHeight);
+      } else {
+        y += CardTopSpriteHeight + 1;
       }
-      x += 4;
-   }
+    }
+
+    x += 4;
+  }
 }
 
-void Drawing::DrawTowers()
-{
-   uint8_t x = TowersLeft;
-
-   for (uint8_t tower=0; tower<4; ++tower)
-   {
-      Card card = Game::instance.GetTower(tower);
-      if (!card.IsNull())
-      {
-         DrawCard(card, x, CardLocation::TowersTop);
-      }
-      x += 4;
-   }
-}
-
-void Drawing::DrawColumns()
-{
-   uint8_t x = 0;
-
-   for (uint8_t i=0; i<10; ++i)
-   {
-      Column &column = Game::instance.GetColumn(i);
-
-      uint8_t y = CardLocation::ColumnsTop;
-      for (uint8_t j=0; j<5; ++j)
-      {
-         DrawCardTop(column.GetCard(j), x, y);
-         if (j == 4)
-         {
-            DrawCardBottom(x, y + CardTopSpriteHeight);
-         }
-         else
-         {
-            y += CardTopSpriteHeight + 1;
-         }
-      }
-
-      x += 4;
-   }
-}
-
-void Drawing::DrawGame()
-{
-   DrawAcePiles();
-   DrawColumns();
-   DrawTowers();
+void Drawing::DrawGame() {
+  DrawAcePiles();
+  DrawColumns();
+  DrawTowers();
 }
 
 
-void Drawing::SaveCardBackground(uint8_t x, uint8_t y, SavedBackground *background)
-{
-   a2::VBLCounter::Update();
-   uint8_t *p = &background->pixels[0];
-   uint8_t *row;
-   for (uint8_t i=0; i<CardHeight; ++i)
-   {
-      row = hgr.GetByteAddress(y++, x);
-      p[0] = row[0];
-      p[1] = row[1];
-      p[2] = row[2];
-      p[3] = row[3];
-      p += 4;
-   }
+void Drawing::SaveCardBackground(
+              uint8_t x, uint8_t y,
+              SavedBackground *background) {
+  a2::VBLCounter::Update();
+  uint8_t *p = &background->pixels[0];
+  uint8_t *row;
+  for (uint8_t i=0; i < CardHeight; ++i) {
+    row = hgr.GetByteAddress(y++, x);
+    p[0] = row[0];
+    p[1] = row[1];
+    p[2] = row[2];
+    p[3] = row[3];
+    p += 4;
+  }
 }
 
 
-void Drawing::RestoreBackground(SavedBackground &background, uint8_t x, uint8_t y)
-{
-   a2::VBLCounter::Update();
-   uint8_t *p = &background.pixels[0];
-   uint8_t *row;
-   for (uint8_t i=0; i<CardHeight; ++i)
-   {
-      row = hgr.GetByteAddress(y++, x);
-      row[0] = p[0];
-      row[1] = p[1];
-      row[2] = p[2];
-      row[3] = p[3];
-      p += 4;
-   }
+void Drawing::RestoreBackground(
+                SavedBackground &background,
+                uint8_t x, uint8_t y) {
+  a2::VBLCounter::Update();
+  uint8_t *p = &background.pixels[0];
+  uint8_t *row;
+  for (uint8_t i=0; i < CardHeight; ++i) {
+    row = hgr.GetByteAddress(y++, x);
+    row[0] = p[0];
+    row[1] = p[1];
+    row[2] = p[2];
+    row[3] = p[3];
+    p += 4;
+  }
 }
-
--- Seahaven2e/Drawing.h
+++ Seahaven2e/Drawing.h
@@ -1,6 +1,9 @@
+// =============================================================
+//    Copyright 2023 Randy Rasmussen
+// =============================================================
 
-#ifndef DRAWING_H
-#define DRAWING_H
+#ifndef SEAHAVEN2E_DRAWING_H_
+#define SEAHAVEN2E_DRAWING_H_
 
 #include <stdint.h>
 #include <Apple2Lib/HGRPage.h>
@@ -9,37 +12,40 @@
 #include "SavedBackground.h"
 
 class Drawing {
-public:
-   constexpr Drawing(a2::HGRPage hgrPage)
-      : hgr(hgrPage)
-   {
-   }
+ public:
+  explicit constexpr Drawing(a2::HGRPage hgrPage)
+    : hgr(hgrPage) {
+  }
 
-   void DrawBackground();
-   void DrawCard(Card card, uint8_t x, uint8_t y);
-   void DrawCardBackground(uint8_t row, uint8_t byteOffset);
-   void SaveCardBackground(uint8_t x, uint8_t y, SavedBackground *background);
-   void DrawGame();
-   void DrawAcePiles();
-   void DrawTowers();
-   void CopyTo(Drawing &target) { hgr.CopyTo(target.hgr); }
-   void RestoreBackground(SavedBackground &background, uint8_t x, uint8_t y);
+  void DrawBackground();
+  void DrawCard(Card card, uint8_t x, uint8_t y);
+  void DrawCardBackground(uint8_t row, uint8_t byteOffset);
+  void SaveCardBackground(uint8_t x, uint8_t y, SavedBackground *background);
+  void DrawGame();
+  void DrawAcePiles();
+  void DrawTowers();
+  void CopyTo(Drawing *target) { hgr.CopyTo(target.hgr); }
+  void RestoreBackground(SavedBackground *background, uint8_t x, uint8_t y);
 
-private:
-   void DrawAcePile(Suit suit, uint8_t x);
-   void DrawCardBottom(uint8_t x, uint8_t y);
-   void DrawCardTop(Card card, uint8_t x, uint8_t y);
-   void DrawColumns();
-   void DrawCursor(uint8_t x, uint8_t y);
-   void DrawSprite(const a2::HGRWord *sprite, uint8_t rows, uint8_t y, uint8_t x);
-   void XorSprite(const a2::HGRWord *sprite, uint8_t rows, uint8_t y, uint8_t x);
+ private:
+  void DrawAcePile(Suit suit, uint8_t x);
+  void DrawCardBottom(uint8_t x, uint8_t y);
+  void DrawCardTop(Card card, uint8_t x, uint8_t y);
+  void DrawColumns();
+  void DrawCursor(uint8_t x, uint8_t y);
+  void DrawSprite(
+          const a2::HGRWord *sprite,
+          uint8_t rows,
+          uint8_t y,
+          uint8_t x);
+  void XorSprite(const a2::HGRWord *sprite, uint8_t rows, uint8_t y, uint8_t x);
 
-private:
-   const a2::HGRPage hgr;
+ private:
+  const a2::HGRPage hgr;
 };
 
 extern Drawing drawing1;
 extern Drawing drawing2;
 
 
-#endif // DRAWING_H
+#endif  // SEAHAVEN2E_DRAWING_H_
