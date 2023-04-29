e9438f52d86c3fb1df36234bc2e9fabf45d55906
notjulie <rasmussenrandy189@gmail.com>
2023-04-29 08:35:34 -0500

    More cpplint

--- Seahaven2e/Sprites.cpp
+++ Seahaven2e/Sprites.cpp
@@ -1,3 +1,6 @@
+// =============================================================
+//    Copyright 2023 Randy Rasmussen
+// =============================================================
 
 #include "Sprites.h"
 
@@ -5,254 +8,250 @@ c6502::Lookup16Bit<const CardTopSprite *, 13> Sprites::ranksLookup;
 c6502::Lookup16Bit<const CardTopSprite *, 4> Sprites::suitsLookup;
 
 const CardTopSprite Sprites::suits[4] {
-   // club
-   {
-      "WWWWWWWWWWWW  ",
-      "WWWWW  WWWWWW ",
-      "WWWW    WWWWW ",
-      "WWWWW  WWWWWW ",
-      "WW        WWW ",
-      "WW        WWW ",
-      "WWWWW  WWWWWW ",
-      "WWWW    WWWWW ",
-      "WWWWWWWWWWWWW ",
-   },
+  // club
+  {
+    "WWWWWWWWWWWW  ",
+    "WWWWW  WWWWWW ",
+    "WWWW    WWWWW ",
+    "WWWWW  WWWWWW ",
+    "WW        WWW ",
+    "WW        WWW ",
+    "WWWWW  WWWWWW ",
+    "WWWW    WWWWW ",
+    "WWWWWWWWWWWWW ",
+  },
 
-   // diamond
-   {
-      "WWWWWWWWWWWW  ",
-      "WWWWWRRRWWWWW ",
-      "WWWWRRRRRWWWW ",
-      "WWWRRRRRRRWWW ",
-      "WWRRRRRRRRRWW ",
-      "WWWRRRRRRRWWW ",
-      "WWWWRRRRRWWWW ",
-      "WWWWWRRRWWWWW ",
-      "WWWWWWWWWWWWW ",
-   },
+  // diamond
+  {
+    "WWWWWWWWWWWW  ",
+    "WWWWWRRRWWWWW ",
+    "WWWWRRRRRWWWW ",
+    "WWWRRRRRRRWWW ",
+    "WWRRRRRRRRRWW ",
+    "WWWRRRRRRRWWW ",
+    "WWWWRRRRRWWWW ",
+    "WWWWWRRRWWWWW ",
+    "WWWWWWWWWWWWW ",
+  },
 
-   // heart
-   {
-      "WWWWWWWWWWWW  ",
-      "WWWRRRWWRRWWW ",
-      "WRRRRRRRRRRRW ",
-      "WWRRRRRRRRRWW ",
-      "WWWRRRRRRRWWW ",
-      "WWWWRRRRRWWWW ",
-      "WWWWWRRRWWWWW ",
-      "WWWWWWWWWWWWW ",
-      "WWWWWWWWWWWWW ",
-   },
+  // heart
+  {
+    "WWWWWWWWWWWW  ",
+    "WWWRRRWWRRWWW ",
+    "WRRRRRRRRRRRW ",
+    "WWRRRRRRRRRWW ",
+    "WWWRRRRRRRWWW ",
+    "WWWWRRRRRWWWW ",
+    "WWWWWRRRWWWWW ",
+    "WWWWWWWWWWWWW ",
+    "WWWWWWWWWWWWW ",
+  },
 
-   // spade
-   {
-      "WWWWWWWWWWWW  ",
-      "WWWWW  WWWWWW ",
-      "WWWW    WWWWW ",
-      "WW        WWW ",
-      "W          WW ",
-      "WW        WWW ",
-      "WWWWW  WWWWWW ",
-      "WWWW    WWWWW ",
-      "WWWWWWWWWWWWW ",
-   }
+  // spade
+  {
+    "WWWWWWWWWWWW  ",
+    "WWWWW  WWWWWW ",
+    "WWWW    WWWWW ",
+    "WW        WWW ",
+    "W          WW ",
+    "WW        WWW ",
+    "WWWWW  WWWWWW ",
+    "WWWW    WWWWW ",
+    "WWWWWWWWWWWWW ",
+  }
 };
 
 const CardTopSprite Sprites::ranks[13] {
-   // ace
-   {
-      " WWWWWWWWWWWWW",
-      "WWWWW WWWWWWWW",
-      "WWWW W WWWWWWW",
-      "WWW WWW WWWWWW",
-      "WW       WWWWW",
-      "WW WWWWW WWWWW",
-      "WW WWWWW WWWWW",
-      "WW WWWWW WWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // two
-   {
-      " WWWWWWWWWWWWW",
-      "WWW    WWWWWWW",
-      "WW WWWW WWWWWW",
-      "WWWWWWWW WWWWW",
-      "WWWWWWW WWWWWW",
-      "WWWW   WWWWWWW",
-      "WW WWWWWWWWWWW",
-      "WW       WWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // three
-   {
-      " WWWWWWWWWWWWW",
-      "WWW    WWWWWWW",
-      "WW WWWW WWWWWW",
-      "WWWWWWWW WWWWW",
-      "WWWWW   WWWWWW",
-      "WWWWWWWW WWWWW",
-      "WW WWWW WWWWWW",
-      "WWW    WWWWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // four
-   {
-      " WWWWWWWWWWWWW",
-      "WWWWWW  WWWWWW",
-      "WWWWW W WWWWWW",
-      "WWWW WW WWWWWW",
-      "WWW WWW WWWWWW",
-      "WW       WWWWW",
-      "WWWWWWW WWWWWW",
-      "WWWWWWW WWWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // five
-   {
-      " WWWWWWWWWWWWW",
-      "WW      WWWWWW",
-      "WW WWWWWWWWWWW",
-      "WW     WWWWWWW",
-      "WWWWWWW WWWWWW",
-      "WWWWWWWW WWWWW",
-      "WWW WWW WWWWWW",
-      "WWWW   WWWWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // six
-   {
-      " WWWWWWWWWWWWW",
-      "WWW     WWWWWW",
-      "WW WWWWWWWWWWW",
-      "WW WWWWWWWWWWW",
-      "WW      WWWWWW",
-      "WW WWWWW WWWWW",
-      "WWW WWWW WWWWW",
-      "WWWW    WWWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // seven
-   {
-      " WWWWWWWWWWWWW",
-      "WW       WWWWW",
-      "WWWWWWWW WWWWW",
-      "WWWWWWW WWWWWW",
-      "WWWWWW WWWWWWW",
-      "WWWWW WWWWWWWW",
-      "WWWW WWWWWWWWW",
-      "WWWW WWWWWWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // eight
-   {
-      " WWWWWWWWWWWWW",
-      "WWW     WWWWWW",
-      "WW WWWWW WWWWW",
-      "WW WWWWW WWWWW",
-      "WWW     WWWWWW",
-      "WW WWWWW WWWWW",
-      "WW WWWWW WWWWW",
-      "WWW     WWWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // nine
-   {
-      " WWWWWWWWWWWWW",
-      "WWW     WWWWWW",
-      "WW WWWWW WWWWW",
-      "WW WWWWW WWWWW",
-      "WWW      WWWWW",
-      "WWWWWWWW WWWWW",
-      "WWWWWWWW WWWWW",
-      "WWW     WWWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // ten
-   {
-      " WWWWWWWWWWWWW",
-      "WWW WWW  WWWWW",
-      "WW  WW WW WWWW",
-      "WWW WW WW WWWW",
-      "WWW WW WW WWWW",
-      "WWW WW WW WWWW",
-      "WWW WW WW WWWW",
-      "WWW WWW  WWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // jack
-   {
-      " WWWWWWWWWWWWW",
-      "WWWWW   WWWWWW",
-      "WWWWWW WWWWWWW",
-      "WWWWWW WWWWWWW",
-      "WWWWWW WWWWWWW",
-      "WWWWWW WWWWWWW",
-      "WW WWW WWWWWWW",
-      "WWW   WWWWWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // queen
-   {
-      " WWWWWWWWWWWWW",
-      "WWWW   WWWWWWW",
-      "WW WWWW WWWWWW",
-      "WW WWWW WWWWWW",
-      "WW WWWW WWWWWW",
-      "WW WW W WWWWWW",
-      "WW WWW  WWWWWW",
-      "WWW   WW WWWWW",
-      "WWWWWWWWWWWWWW",
-   },
-   // king
-   {
-      " WWWWWWWWWWWWW",
-      "WWW WWW WWWWWW",
-      "WWW WW WWWWWWW",
-      "WWW W WWWWWWWW",
-      "WWW  WWWWWWWWW",
-      "WWW  W WWWWWWW",
-      "WWW WWW WWWWWW",
-      "WWW WWWW WWWWW",
-      "WWWWWWWWWWWWWW",
-   },
+  // ace
+  {
+    " WWWWWWWWWWWWW",
+    "WWWWW WWWWWWWW",
+    "WWWW W WWWWWWW",
+    "WWW WWW WWWWWW",
+    "WW       WWWWW",
+    "WW WWWWW WWWWW",
+    "WW WWWWW WWWWW",
+    "WW WWWWW WWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // two
+  {
+    " WWWWWWWWWWWWW",
+    "WWW    WWWWWWW",
+    "WW WWWW WWWWWW",
+    "WWWWWWWW WWWWW",
+    "WWWWWWW WWWWWW",
+    "WWWW   WWWWWWW",
+    "WW WWWWWWWWWWW",
+    "WW       WWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // three
+  {
+    " WWWWWWWWWWWWW",
+    "WWW    WWWWWWW",
+    "WW WWWW WWWWWW",
+    "WWWWWWWW WWWWW",
+    "WWWWW   WWWWWW",
+    "WWWWWWWW WWWWW",
+    "WW WWWW WWWWWW",
+    "WWW    WWWWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // four
+  {
+    " WWWWWWWWWWWWW",
+    "WWWWWW  WWWWWW",
+    "WWWWW W WWWWWW",
+    "WWWW WW WWWWWW",
+    "WWW WWW WWWWWW",
+    "WW       WWWWW",
+    "WWWWWWW WWWWWW",
+    "WWWWWWW WWWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // five
+  {
+    " WWWWWWWWWWWWW",
+    "WW      WWWWWW",
+    "WW WWWWWWWWWWW",
+    "WW     WWWWWWW",
+    "WWWWWWW WWWWWW",
+    "WWWWWWWW WWWWW",
+    "WWW WWW WWWWWW",
+    "WWWW   WWWWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // six
+  {
+    " WWWWWWWWWWWWW",
+    "WWW     WWWWWW",
+    "WW WWWWWWWWWWW",
+    "WW WWWWWWWWWWW",
+    "WW      WWWWWW",
+    "WW WWWWW WWWWW",
+    "WWW WWWW WWWWW",
+    "WWWW    WWWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // seven
+  {
+    " WWWWWWWWWWWWW",
+    "WW       WWWWW",
+    "WWWWWWWW WWWWW",
+    "WWWWWWW WWWWWW",
+    "WWWWWW WWWWWWW",
+    "WWWWW WWWWWWWW",
+    "WWWW WWWWWWWWW",
+    "WWWW WWWWWWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // eight
+  {
+    " WWWWWWWWWWWWW",
+    "WWW     WWWWWW",
+    "WW WWWWW WWWWW",
+    "WW WWWWW WWWWW",
+    "WWW     WWWWWW",
+    "WW WWWWW WWWWW",
+    "WW WWWWW WWWWW",
+    "WWW     WWWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // nine
+  {
+    " WWWWWWWWWWWWW",
+    "WWW     WWWWWW",
+    "WW WWWWW WWWWW",
+    "WW WWWWW WWWWW",
+    "WWW      WWWWW",
+    "WWWWWWWW WWWWW",
+    "WWWWWWWW WWWWW",
+    "WWW     WWWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // ten
+  {
+    " WWWWWWWWWWWWW",
+    "WWW WWW  WWWWW",
+    "WW  WW WW WWWW",
+    "WWW WW WW WWWW",
+    "WWW WW WW WWWW",
+    "WWW WW WW WWWW",
+    "WWW WW WW WWWW",
+    "WWW WWW  WWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // jack
+  {
+    " WWWWWWWWWWWWW",
+    "WWWWW   WWWWWW",
+    "WWWWWW WWWWWWW",
+    "WWWWWW WWWWWWW",
+    "WWWWWW WWWWWWW",
+    "WWWWWW WWWWWWW",
+    "WW WWW WWWWWWW",
+    "WWW   WWWWWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // queen
+  {
+    " WWWWWWWWWWWWW",
+    "WWWW   WWWWWWW",
+    "WW WWWW WWWWWW",
+    "WW WWWW WWWWWW",
+    "WW WWWW WWWWWW",
+    "WW WW W WWWWWW",
+    "WW WWW  WWWWWW",
+    "WWW   WW WWWWW",
+    "WWWWWWWWWWWWWW",
+  },
+  // king
+  {
+    " WWWWWWWWWWWWW",
+    "WWW WWW WWWWWW",
+    "WWW WW WWWWWWW",
+    "WWW W WWWWWWWW",
+    "WWW  WWWWWWWWW",
+    "WWW  W WWWWWWW",
+    "WWW WWW WWWWWW",
+    "WWW WWWW WWWWW",
+    "WWWWWWWWWWWWWW",
+  },
 };
 
 
-const CardTopSprite Sprites::cursorLeft =
-{
-   "              ",
-   "              ",
-   "              ",
-   "           www",
-   "           www",
-   "           www",
-   "              ",
-   "              ",
-   "              ",
+const CardTopSprite Sprites::cursorLeft = {
+  "              ",
+  "              ",
+  "              ",
+  "           www",
+  "           www",
+  "           www",
+  "              ",
+  "              ",
+  "              ",
 };
 
-const CardTopSprite Sprites::cursorRight =
-{
-   "              ",
-   "              ",
-   "              ",
-   "w             ",
-   "w             ",
-   "w             ",
-   "              ",
-   "              ",
-   "              ",
+const CardTopSprite Sprites::cursorRight = {
+  "              ",
+  "              ",
+  "              ",
+  "w             ",
+  "w             ",
+  "w             ",
+  "              ",
+  "              ",
+  "              ",
 };
 
 
-void Sprites::Initialize()
-{
-   const CardTopSprite *rankSprite = &ranks[0];
-   for (uint8_t i=0; i<13; ++i)
-      ranksLookup.Set(i, rankSprite++);
+void Sprites::Initialize() {
+  const CardTopSprite *rankSprite = &ranks[0];
+  for (uint8_t i=0; i < 13; ++i)
+    ranksLookup.Set(i, rankSprite++);
 
-   const CardTopSprite *suitSprite = &suits[0];
-   for (uint8_t i=0; i<4; ++i)
-      suitsLookup.Set(i, suitSprite++);
+  const CardTopSprite *suitSprite = &suits[0];
+  for (uint8_t i=0; i < 4; ++i)
+    suitsLookup.Set(i, suitSprite++);
 }
-
--- Seahaven2e/Sprites.h
+++ Seahaven2e/Sprites.h
@@ -1,6 +1,9 @@
+// =============================================================
+//    Copyright 2023 Randy Rasmussen
+// =============================================================
 
-#ifndef SPRITES_H
-#define SPRITES_H
+#ifndef SEAHAVEN2E_SPRITES_H_
+#define SEAHAVEN2E_SPRITES_H_
 
 #include <stdint.h>
 #include <C6502/LookupTable.h>
@@ -15,25 +18,25 @@ typedef a2::HGRWord CardTopSprite[CardTopSpriteHeight];
 
 
 class Sprites {
-public:
-   static void Initialize();
+ public:
+  static void Initialize();
 
-   inline static const CardTopSprite &GetRankSprite(Rank rank) {
-      return *ranksLookup.Get((uint8_t)rank - 1);
-   }
-   inline static const CardTopSprite &GetSuitSprite(Suit suit) {
-      return *suitsLookup.Get((uint8_t)suit);
-   }
+  inline static const CardTopSprite &GetRankSprite(Rank rank) {
+    return *ranksLookup.Get((uint8_t)rank - 1);
+  }
+  inline static const CardTopSprite &GetSuitSprite(Suit suit) {
+    return *suitsLookup.Get((uint8_t)suit);
+  }
 
-public:
-   static const CardTopSprite ranks[13];
-   static c6502::Lookup16Bit<const CardTopSprite *, 13> ranksLookup;
+ public:
+  static const CardTopSprite ranks[13];
+  static c6502::Lookup16Bit<const CardTopSprite *, 13> ranksLookup;
 
-   static const CardTopSprite suits[4];
-   static c6502::Lookup16Bit<const CardTopSprite *, 4> suitsLookup;
+  static const CardTopSprite suits[4];
+  static c6502::Lookup16Bit<const CardTopSprite *, 4> suitsLookup;
 
-   static const CardTopSprite cursorLeft;
-   static const CardTopSprite cursorRight;
+  static const CardTopSprite cursorLeft;
+  static const CardTopSprite cursorRight;
 };
 
-#endif // SPRITES_H
+#endif  // SEAHAVEN2E_SPRITES_H_
