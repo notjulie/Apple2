317b851917662d3eec5c96b3a98e40a5e4661800
notjulie <rasmussenrandy189@gmail.com>
2023-08-01 23:19:59 -0500

    Changed the linker script to include a handy jump to an exit function in case I want to bail in the debugger anc clean up the screen,

--- Seahaven2e/main.cpp
+++ Seahaven2e/main.cpp
@@ -21,23 +21,43 @@
 static StateMachine stateMachine;
 
 
-/** \brief
- * Main entry point
- */
-extern "C" int main() {
-  // call initializers
-  Sprites::Initialize();
-  PersistentState::instance = PersistentState();
-  stateMachine = StateMachine();
-  CardAnimator::instance.Initialize();
-  Cursor::instance = Cursor();
+/// <summary>
+/// Main entry point
+/// </summary>
+extern "C" int main()
+{
+   // call initializers
+   Sprites::Initialize();
+   PersistentState::instance = PersistentState();
+   stateMachine = StateMachine();
+   CardAnimator::instance.Initialize();
+   Cursor::instance = Cursor();
 
-  // enter main loop
-  for (;;) {
-    // update the VBL counter
-    a2::VBLCounter::Update();
+   // enter main loop
+   for (;;)
+   {
+     // update the VBL counter
+     a2::VBLCounter::Update();
 
-    // give the state machine its timeslice
-    stateMachine.Service();
-  }
+     // give the state machine its timeslice
+     stateMachine.Service();
+   }
+}
+
+
+/// <summary>
+/// Alternate entry point that just cleans up the screen and exits;
+/// I can use this in AppleWin to break into the BASIC command line
+/// so that I can BSAVE to a disk image.  Basically, I just code a
+/// JMP to this at $6004 so that if I pop into the AppleWin debugger
+/// I can jump here, and it will clean up the screen and take me to
+/// the BASIC prompt, at which point I can use DOS commands to save
+/// the binary to a disk.  It's a little Rube-Goldberg.  And
+/// therefore awesome.
+/// </summary>
+extern "C" void exit()
+{
+   a2::PAGE2OFF();
+   a2::TEXTON();
+   a2::DOSWRM();
 }
