fbbaa53e80881fb2c3a13ab4eff2acbaaf5e44a1
maigashi <rasmussenrandy189@gmail.com>
2024-01-31 18:48:04 -0600

    Initial framework of a Apple2 Disk utility app

---
+++ A2DiskUtil/DiskFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2DiskUtil
+{
+   /// <summary>
+   /// Representation of a disk image
+   /// </summary>
+   class DiskFile
+   {
+      private byte[] fileData;
+
+      /// <summary>
+      /// Initializes a new disk image from a file
+      /// </summary>
+      /// <param name="filename"></param>
+      public DiskFile(string filename)
+      {
+         fileData = File.ReadAllBytes(filename);
+      }
+   }
+}
---
+++ A2DiskUtil/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A2DiskUtil
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      /// <summary>
+      /// Initializes a new instance of class MainWindow
+      /// </summary>
+      public MainWindow()
+      {
+         // normal component initialization
+         InitializeComponent();
+
+         // add event handlers
+         menuitemOpenDisk.Click += MenuitemOpenDisk_Click;
+      }
+
+      /// <summary>
+      /// Handles the File>Open Disk... item
+      /// </summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      private void MenuitemOpenDisk_Click(object sender, RoutedEventArgs e)
+      {
+         OpenFileDialog dialog = new OpenFileDialog();
+         if (dialog.ShowDialog() == true)
+         {
+            try
+            {
+               DiskFile newFile = new DiskFile(dialog.FileName);
+               DataContext = newFile;
+            }
+            catch (Exception ex) 
+            {
+               MessageBox.Show(ex.Message);
+            }
+         }
+      }
+   }
+}
